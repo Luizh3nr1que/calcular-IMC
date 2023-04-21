3b8078a6c58fd463d261dfc3a611b3d68615d9a1
luizh3nrique <luizh3nrique00@gmail.com>
2023-04-20 21:14:55 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- java.js
+++ java.js
@@ -5,6 +5,7 @@ document.getElementById('button').addEventListener('click', () => {
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
     const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value
 
     const res = document.querySelector('.res');
     const erro = document.querySelector('.erro');
@@ -17,27 +18,27 @@ document.getElementById('button').addEventListener('click', () => {
         const imc = peso / Math.pow(altura, 2)
 
         if (imc < 18.5) {
-            res.innerHTML = `${nome} Está Abaixo do peso`;
+            res.innerHTML = `Continue se cuidando, ${nome}, você está no caminho certo para uma vida saudável!`;
             res.classList.add('normal');
             res.classList.remove('medo');
             res.classList.remove('pesado');
         } else if (imc >= 18.5 && imc < 25) {
-            res.innerHTML = `${nome} Está com peso padrão`;
+            res.innerHTML = `Parabéns, ${nome}, você está no peso ideal para uma vida saudável!`;
             res.classList.add('normal');
             res.classList.remove('medo');
             res.classList.remove('pesado');
         } else if (imc >= 25 && imc < 30) {
-            res.innerHTML = `${nome} está acima do peso`;
+            res.innerHTML = `Hora de se cuidar, ${nome}, vamos juntos nessa jornada para uma vida mais saudável!`;
             res.classList.add('medo');
             res.classList.remove('normal');
             res.classList.remove('pesado');
         } else if (imc >= 30 && imc < 40) {
-            res.innerHTML = `${nome} está Obeso`;
+            res.innerHTML = `Atenção!, ${nome}, é hora de procurar ajuda e cuidar da sua saúde!`;
             res.classList.add('pesado');
             res.classList.remove('medo');
             res.classList.remove('normal');
         } else {
-            res.innerHTML = `${nome} está com Obesidade grave`;
+            res.innerHTML = `Urgente!!! ${nome}, procure ajuda médica imediatamente para tratar sua obesidade grave.`;
             res.classList.add('pesado');
             res.classList.remove('medo');
             res.classList.remove('normal');
@@ -59,7 +60,10 @@ document.getElementById('limpar').addEventListener('click', () => {
         aviso.classList.add('pesado');
         aviso.style.marginTop = '10px';
     } else {
-        location.reload(5000)
+        peso.value = '';
+        altura.value = '';
+        nome.value = '';
+        idade.value = '';
     }
 
 })
@@ -72,3 +76,17 @@ document.querySelector('.imc').addEventListener('click', () => {
         txt.style.display = 'block'
     }
 })
+
+
+const btnMasculino = document.querySelector('.masc');
+const btnFeminino = document.querySelector('.fem');
+
+btnMasculino.addEventListener('click', () => {
+    btnMasculino.classList.add('selecionadoM');
+    btnFeminino.classList.remove('selecionadoF');
+});
+
+btnFeminino.addEventListener('click', () => {
+    btnFeminino.classList.add('selecionadoF');
+    btnMasculino.classList.remove('selecionadoM');
+});
